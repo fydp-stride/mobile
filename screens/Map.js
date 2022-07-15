1fced5738ce48e490b88f53e4a02cd6e472694c3
Kevin Chen <kevinchen828@hotmail.com>
2022-07-14 22:04:32 -0400

    maps wip

--- screens/Map.js
+++ screens/Map.js
@@ -5,9 +5,15 @@ import {
   StatusBar,
   StyleSheet,
   useColorScheme,
+  Button,
 } from 'react-native';
 import { Layout, Text } from '@ui-kitten/components';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+import BackgroundGeolocation, {
+  Location,
+  Subscription,
+} from 'react-native-background-geolocation';
 
 const styles = StyleSheet.create({
   container: {
@@ -23,26 +29,95 @@ const styles = StyleSheet.create({
 });
 
 const Map = ({ navigation }) => {
+  const [location, setLocation] = React.useState('');
+  const [locArr, setLocArr] = React.useState([]);
+  const [tracking, setTracking] = React.useState(false);
+  const [trackInterval, setTrackInterval] = React.useState(null);
+
+  React.useEffect(() => {
+    const getLocation = async () => {
+      let loc = await BackgroundGeolocation.getCurrentPosition({
+        timeout: 30, // 30 second timeout to fetch location
+        maximumAge: 5000, // Accept the last-known-location if not older than 5000 ms.
+        desiredAccuracy: 10, // Try to fetch a location with an accuracy of `10` meters.
+        samples: 3, // How many location samples to attempt.
+        extras: {
+          // Custom meta-data.
+          route_id: 123,
+        },
+      });
+      console.log('[getLocation]', loc.coords);
+      setLocation(loc);
+    };
+
+    getLocation().catch(e => console.error(e));
+  }, []);
+
+  const INTERVAL = 15000; // 10 s
+  const startRecordingLoc = async () => {
+    setTracking(true);
+    const interval = setInterval(async () => {
+      let cur = await BackgroundGeolocation.getCurrentPosition({
+        timeout: 15, // 30 second timeout to fetch location
+        maximumAge: 5000, // Accept the last-known-location if not older than 5000 ms.
+        desiredAccuracy: 10, // Try to fetch a location with an accuracy of `10` meters.
+        samples: 3, // How many location samples to attempt.
+        extras: {
+          // Custom meta-data.
+          route_id: 123,
+        },
+      });
+      locArr.push(cur.coords);
+      setLocArr(locArr);
+      console.log(locArr);
+    }, INTERVAL);
+    setTrackInterval(interval);
+  };
+
+  const stopRecordingLoc = () => {
+    setTracking(false);
+    clearInterval(trackInterval);
+    console.log(locArr);
+    setLocArr([]);
+  };
+
   return (
     <Layout
       style={{
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  }}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  </Layout>
+        ...StyleSheet.absoluteFillObject,
+        height: 600,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}>
+      {location && (
+        <>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            showsUserLocation={true}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }}
+          />
+          {!tracking ? (
+            <Button
+              title="Click to start recording"
+              onPress={startRecordingLoc}
+            />
+          ) : (
+            <Button
+              title="Click to stop recording"
+              onPress={stopRecordingLoc}
+            />
+          )}
+        </>
+      )}
+    </Layout>
+  );
 };
 
 export default Map;
