9adf9fff2539183c9ac8b6f29e5fb26716d7bddc
Kevin Chen <kevinchen828@hotmail.com>
2023-01-24 00:39:14 -0500

    Profile page WIP

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,20 @@
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, Button } from '@ui-kitten/components';
 import * as React from 'react';
-// import { View, Text, Image, ImageBackground } from 'react-native';
+import { View, Image, ImageBackground } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <Layout style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text style={{ fontSize: 30 }}>
-        This is homescreen
+    <Layout style={{ alignItems: 'center', flex: 1, backgroundColor: 'white' }}>
+      <View style={{ backgroundColor: 'lightgreen', height: '35%', width: '100%', position: 'absolute' }} ></View>
+      <Text style={{ fontSize: 30, color: 'black', margin: 20, paddingBottom: 15 }}>
+        Profile
       </Text>
+      <View style={{ zIndex: 999, borderWidth: 0, borderRadius: 100, height: 155, width: 155, backgroundColor: 'black', justifyContent: 'center' }}></View>
+      <Text style={{ fontSize: 30, color: 'black', marginTop: 20 }}>Welcome Back, Bob</Text>
+      <Text style={{ fontSize: 18, color: 'black', marginTop: 10, marginBottom: 30}}>You are doing great today!</Text>
+      <Button onPress={() => {}} style={{ backgroundColor: 'green', borderWidth: 0, borderRadius: 100, width: '60%', marginBottom: 20 }}>Modify Biometrics</Button>
+      <Button onPress={() => {}} style={{ backgroundColor: 'green', borderWidth: 0, borderRadius: 100, width: '60%', marginBottom: 20 }}>Bluetooth Connection</Button>
+      <Button onPress={() => {}} style={{ backgroundColor: 'green', borderWidth: 0, borderRadius: 100, width: '60%', marginBottom: 20 }}>Logout</Button>
     </Layout>
   );
 }
